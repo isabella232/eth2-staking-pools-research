f1cf393e0479428a092e705719f12d059aa5daed
Alon Muroch <alonmuroch@gmail.com>
2020-06-30 14:14:38 +0300

    ecc interpolation

---
+++ go_minimal_pool/crypto/ecc_polynomial.go
@@ -0,0 +1,71 @@
+package crypto
+
+import "github.com/herumi/bls-eth-go-binary/bls"
+
+type ECCG1Polynomial struct {
+	G1Points []bls.G1
+	XPoints []bls.Fr
+}
+
+func NewG1LagrangeInterpolation(points [][]interface{}) *ECCG1Polynomial {
+	x := make([]bls.Fr, len(points))
+	y := make([]bls.G1, len(points))
+	for i := range points {
+		point := points[i]
+		x[i] = point[0].(bls.Fr)
+		y[i] = *point[1].(*bls.G1)
+	}
+
+	return &ECCG1Polynomial{
+		G1Points:y,
+		XPoints:x,
+	}
+}
+
+func (p *ECCG1Polynomial) interpolate() (*bls.G1,error) {
+	res := &bls.G1{}
+	err := bls.G1LagrangeInterpolation(res, p.XPoints, p.G1Points)
+	if err != nil {
+		return nil, err
+	}
+	return res,nil
+}
+
+
+
+
+
+
+
+
+
+
+
+type ECCG2Polynomial struct {
+	G2Points []bls.G2
+	XPoints []bls.Fr
+}
+
+func NewG2LagrangeInterpolation(points [][]interface{}) *ECCG2Polynomial {
+	x := make([]bls.Fr, len(points))
+	y := make([]bls.G2, len(points))
+	for i := range points {
+		point := points[i]
+		x[i] = point[0].(bls.Fr)
+		y[i] = *point[1].(*bls.G2)
+	}
+
+	return &ECCG2Polynomial{
+		G2Points:y,
+		XPoints:x,
+	}
+}
+
+func (p *ECCG2Polynomial) interpolate() (*bls.G2,error) {
+	res := &bls.G2{}
+	err := bls.G2LagrangeInterpolation(res, p.XPoints, p.G2Points)
+	if err != nil {
+		return nil, err
+	}
+	return res,nil
+}
---
+++ go_minimal_pool/crypto/ecc_polynomial_test.go
@@ -0,0 +1,101 @@
+package crypto
+
+import (
+	"github.com/herumi/bls-eth-go-binary/bls"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func g1FromFr(fr bls.Fr) *bls.G1 {
+	sk := &bls.SecretKey{}
+	sk.SetDecString(fr.GetString(10))
+
+	pk := sk.GetPublicKey()
+	return bls.CastFromPublicKey(pk)
+}
+
+func g2FromFr(fr bls.Fr) *bls.G2 {
+	sk := &bls.SecretKey{}
+	sk.SetDecString(fr.GetString(10))
+
+	sig := sk.Sign("test")
+	return bls.CastFromSign(sig)
+}
+
+func TestG2Interpolation(t *testing.T) {
+	initBLS()
+
+	// generate a polynomial
+	poly := Polynomial{
+		Degree:              2,
+		Coefficients:        []bls.Fr{
+			frFromInt(6), // free coefficient
+			frFromInt(1), // x^2
+			frFromInt(0), // x^1
+		},
+	}
+
+	// evaluate it and transform to g1 results
+	res1,err := poly.Evaluate(1)
+	require.NoError(t, err)
+	res2,err := poly.Evaluate(2)
+	require.NoError(t, err)
+	res3,err := poly.Evaluate(3)
+	require.NoError(t, err)
+	p1 := g2FromFr(*res1)
+	p2 := g2FromFr(*res2)
+	p3 := g2FromFr(*res3)
+
+	// interpolate back to a polynomial
+	points := [][]interface{} {
+		{frFromInt(1),p1},
+		{frFromInt(2),p2},
+		{frFromInt(3),p3},
+	}
+	l := NewG2LagrangeInterpolation(points)
+	res,err := l.interpolate()
+	require.NoError(t, err)
+
+	// compare results
+	expected := g2FromFr(frFromInt(6))
+	require.Equal(t, expected.GetString(10), res.GetString(10))
+}
+
+func TestG1Interpolation(t *testing.T) {
+	initBLS()
+
+	// generate a polynomial
+	poly := Polynomial{
+		Degree:              2,
+		Coefficients:        []bls.Fr{
+			frFromInt(6), // free coefficient
+			frFromInt(1), // x^2
+			frFromInt(0), // x^1
+		},
+	}
+
+	// evaluate it and transform to g1 results
+	res1,err := poly.Evaluate(1)
+	require.NoError(t, err)
+	res2,err := poly.Evaluate(2)
+	require.NoError(t, err)
+	res3,err := poly.Evaluate(3)
+	require.NoError(t, err)
+	p1 := g1FromFr(*res1)
+	p2 := g1FromFr(*res2)
+	p3 := g1FromFr(*res3)
+
+	// interpolate back to a polynomial
+	points := [][]interface{} {
+		{frFromInt(1),p1},
+		{frFromInt(2),p2},
+		{frFromInt(3),p3},
+	}
+	l := NewG1LagrangeInterpolation(points)
+	res,err := l.interpolate()
+	require.NoError(t, err)
+
+	// compare results
+	expected := g1FromFr(frFromInt(6))
+	require.Equal(t, expected.GetString(10), res.GetString(10))
+}
