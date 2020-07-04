8b5fe7f87f6eeaf4c798d4f7f8054c3cf81a4b16
Alon Muroch <alonmuroch@gmail.com>
2020-07-04 08:10:49 +0300

    dkg for groups

--- go_minimal_pool/crypto/dkg.go
+++ go_minimal_pool/crypto/dkg.go
@@ -6,28 +6,54 @@ import "github.com/herumi/bls-eth-go-binary/bls"
 	This builds a polynomial for a particular secret and generates shares for distribution
  */
 type DKG struct {
-	polynomial *Polynomial
+	polynomials map[*bls.Fr]*Polynomial
+	degree uint8
 }
 
-func NewDKG(secret bls.Fr, degree uint8) (*DKG,error) {
-	p, err := NewPolynomial(secret, degree)
-	if err != nil {
-		return nil, err
-	}
-
-	return &DKG{polynomial:p}, nil
-}
-
-func (dkg *DKG) shares(indexes []*bls.Fr) (map[string]*bls.Fr, error) {
-	ret := make(map[string]*bls.Fr)
+func NewDKG(degree uint8, indexes []*bls.Fr) (*DKG,error) {
+	polynomials := make(map[*bls.Fr]*Polynomial)
 	for _, idx := range indexes {
-		p,err := dkg.polynomial.Evaluate(idx)
+		secret := &bls.Fr{}
+		secret.SetByCSPRNG()
+		p, err := NewPolynomial(*secret, degree)
 		if err != nil {
 			return nil, err
 		}
 
-		ret[idx.GetString(10)] = p
+		polynomials[idx] = p
 	}
 
-	return ret, nil
+	return &DKG{polynomials:polynomials, degree:degree}, nil
+}
+
+func (dkg *DKG) GroupSecrets(indexes []*bls.Fr) (map[*bls.Fr]*bls.Fr, error) {
+	ret := make(map[*bls.Fr][]*bls.Fr)
+	for p_idx := range dkg.polynomials {
+		poly := dkg.polynomials[p_idx]
+		for _, share_idx := range indexes {
+			p,err := poly.Evaluate(share_idx)
+			if err != nil {
+				return nil, err
+			}
+
+			ret[share_idx] = append(ret[share_idx], p)
+		}
+	}
+
+	return dkg.sumShares(ret), nil
+}
+
+func (dkg *DKG) sumShares(shares map[*bls.Fr][]*bls.Fr) map[*bls.Fr]*bls.Fr {
+	ret := make(map[*bls.Fr]*bls.Fr)
+	for pIdx, shares := range shares {
+		sum := &bls.Fr{}
+		sum.SetInt64(0)
+		for _, s := range shares {
+			bls.FrAdd(sum, sum, s)
+		}
+
+		ret[pIdx] = sum
+	}
+
+	return ret
 }
--- go_minimal_pool/crypto/dkg_test.go
+++ go_minimal_pool/crypto/dkg_test.go
@@ -10,7 +10,7 @@ func TestDKG(t *testing.T) {
 	initBLS()
 
 	// generate a polynomial
-	poly := &Polynomial{
+	poly1 := &Polynomial{
 		Degree: 2,
 		Coefficients: []bls.Fr{
 			frFromInt(6), // free coefficient
@@ -18,22 +18,42 @@ func TestDKG(t *testing.T) {
 			frFromInt(1), // x^2
 		},
 	}
+	poly2 := &Polynomial{
+		Degree: 2,
+		Coefficients: []bls.Fr{
+			frFromInt(3), // free coefficient
+			frFromInt(0), // x^1
+			frFromInt(1), // x^2
+		},
+	}
+	poly3 := &Polynomial{
+		Degree: 2,
+		Coefficients: []bls.Fr{
+			frFromInt(9), // free coefficient
+			frFromInt(0), // x^1
+			frFromInt(1), // x^2
+		},
+	}
 
-	dkg := DKG{polynomial:poly}
+	dkg := DKG{polynomials:map[*bls.Fr]*Polynomial{
+		frPointerFromInt(1): poly1,
+		frPointerFromInt(2): poly2,
+		frPointerFromInt(3): poly3,
+	}, degree:2}
 
-	// gete shares
-	shares,err := dkg.shares([]*bls.Fr{
-		frPointerFromInt(1),
-		frPointerFromInt(2),
-		frPointerFromInt(3),
-		frPointerFromInt(4),
+	// gete sks
+	idx1 := frPointerFromInt(1)
+	idx2 := frPointerFromInt(2)
+	idx3 := frPointerFromInt(3)
+	sks,err := dkg.GroupSecrets([]*bls.Fr{
+		idx1,
+		idx2,
+		idx3,
 	})
 	require.NoError(t, err)
-
 	// verify
-	require.Len(t, shares, 4)
-	require.Equal(t, "7", shares["1"].GetString(10))
-	require.Equal(t, "10", shares["2"].GetString(10))
-	require.Equal(t, "15", shares["3"].GetString(10))
-	require.Equal(t, "22", shares["4"].GetString(10))
+	require.Len(t, sks, 3)
+	require.Equal(t, "21", sks[idx1].GetString(10))
+	require.Equal(t, "30", sks[idx2].GetString(10))
+	require.Equal(t, "45", sks[idx3].GetString(10))
 }
