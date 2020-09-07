0b64f9c5e1cb9a9035b787fb7d5fb97fe538b3c2
Alon Muroch <alonmuroch@gmail.com>
2020-09-07 11:53:34 +0300

    renamings

--- go-spec/src/execution.go
+++ go-spec/src/execution.go
@@ -1,5 +1,12 @@
 package src
 
+type BeaconDuty struct {
+	Type				uint8 // 0 - attestation, 1 - block proposal
+	Slot				uint64
+	Included			bool // whether or not it was included in the beacon chain (the pool earned reward from it)
+	Executors			[16]byte // 128 bit of the executors (by order) which executed this duty
+}
+
 type PoolExecutionSummary struct {
 	PoolId        	uint64
 	StartingEpoch 	uint64 // a.k.a previous epoch
