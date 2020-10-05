a764551296781b1e0b720212c7b50770c265cc8f
Alon Muroch <alonmuroch@gmail.com>
2020-10-05 08:41:48 +0300

    cleanup + shuffle tests fix

--- go-spec/src/state_transition/apply_block_test.go
+++ go-spec/src/state_transition/apply_block_test.go
@@ -44,7 +44,14 @@ func TestBlockApplyConsistency(t *testing.T) {
 	require.NoError(t, bls.SetETHmode(bls.EthModeDraft07))
 
 	state := generateTestState(t)
-	_, body := GenerateValidHeadAndBody(state)
+	body := &core.BlockBody{
+		Proposer:             2713,
+		Slot:                 35,
+		ParentBlockRoot:      toByte("75141b2e032f1b045ab9c7998dfd7238044e40eed0b2c526c33340643e871e40"),
+		Randao:               toByte("97c4116516e77c522344aa3c3c223db0c14bad05aa005be63aadd19341e0cc6d"),
+		NewPoolReq:           nil,
+		Attestations:         generateAttestations(state,128, 35,0,true, 0 /* attestation */),
+	}
 
 	preRoot,err := ssz.HashTreeRoot(state)
 	require.NoError(t, err)
